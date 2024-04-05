65630cf59c796ee821df23a13844914ab74a521c
Alberto Basalo <albertobasalo@hotmail.com>
2024-04-05 12:39:01 +0200

    feat: init blockchain traits

--- 4-traits/src/main.rs
+++ 4-traits/src/main.rs
@@ -12,7 +12,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 /// * `data` - A `String` that holds the data of the block.
 /// * `previous_hash` - A `String` that holds the hash of the previous block.
 /// * `hash` - A `String` that holds the hash of the current block.
-#[derive(Debug, Clone, Hash)]
+#[derive(Debug, Clone)]
 struct Block {
     index: usize,
     timestamp: u128,
@@ -66,6 +66,15 @@ trait Mineable {
 
 /* Implementations */
 
+impl Hash for Block {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.index.hash(state);
+        self.timestamp.hash(state);
+        self.data.hash(state);
+        self.previous_hash.hash(state);
+    }
+}
+
 impl Hashable for Block {
     fn get_hash(&self) -> String {
         let mut hasher = DefaultHasher::new();
@@ -74,7 +83,17 @@ impl Hashable for Block {
     }
 
     fn is_valid(&self) -> bool {
-        self.hash.starts_with("0000")
+        // Check if the hash of the block is valid
+        // and print a message if it is not explaining why
+        let hash = self.get_hash();
+        if self.hash != hash {
+            println!(
+                "Block {} hash {} is not the same {}",
+                self.index, self.hash, hash
+            );
+            return false;
+        }
+        true
     }
 }
 
@@ -94,11 +113,11 @@ impl Mineable for Blockchain {
             hash: "".to_string(),
         };
         new_block.hash = new_block.get_hash();
-        self.timestamp = timestamp;
         self.add_block(new_block);
     }
 
     fn add_block(&mut self, block: Block) {
+        self.timestamp = block.timestamp;
         self.blocks.push(block);
     }
 
@@ -107,23 +126,30 @@ impl Mineable for Blockchain {
     }
 
     fn create_genesis_block() -> Block {
-        Block {
+        let mut genesis = Block {
             index: 0,
             timestamp: 0,
             data: "Genesis Block".to_string(),
             previous_hash: "".to_string(),
             hash: "".to_string(),
-        }
+        };
+        genesis.hash = genesis.get_hash();
+        genesis
     }
 
     fn is_valid(&self) -> bool {
         for (index, block) in self.blocks.iter().enumerate() {
             if !block.is_valid() {
+                println!("Block {} is not valid", index);
                 return false;
             }
             if index > 0 {
                 let previous_block = &self.blocks[index - 1];
                 if block.previous_hash != previous_block.hash {
+                    println!(
+                        "Block {} previous hash {} is not the same as block {} hash {}",
+                        block.index, block.previous_hash, previous_block.index, previous_block.hash
+                    );
                     return false;
                 }
             }
@@ -149,4 +175,13 @@ fn main() {
     }
     // Check the validity of the blockchain
     println!("Is the blockchain valid? {}", blockchain.is_valid());
+    // Change the data of a block to make it invalid
+    blockchain.blocks[2].data = "Changed data on block 2".to_string();
+    // Check the validity of the blockchain
+    println!("Is the blockchain still valid? {}", blockchain.is_valid());
+    // Change the data and hash of a block to try to make it valid
+    blockchain.blocks[2].data = "Changed data and hash".to_string();
+    blockchain.blocks[2].hash = blockchain.blocks[2].get_hash();
+    // Check the validity of the blockchain
+    println!("Is the blockchain still valid? {}", blockchain.is_valid());
 }
